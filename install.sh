aa1f323ae4d30e9c4cb7e8f27eb61972dc6df55e
tbaldur <97204751+tbaldur@users.noreply.github.com>
2022-06-26 18:51:20 +0100

    Update install.sh
    More OS supported
    Fetched from cyberpanel website

--- install.sh
+++ install.sh
@@ -16,16 +16,26 @@ elif echo $OUTPUT | grep -q "CloudLinux 7" ; then
 yum install curl wget -y 1> /dev/null
 yum update curl wget ca-certificates -y 1> /dev/null
                 SERVER_OS="CloudLinux"
+elif echo $OUTPUT | grep -q "AlmaLinux 8" ; then
+echo "Checking and installing curl and wget"
+yum install curl wget -y 1> /dev/null
+yum update curl wget ca-certificates -y 1> /dev/null
+        SERVER_OS="AlmaLinux"
+elif echo $OUTPUT | grep -q "Rocky Linux" ; then
+echo "Checking and installing curl and wget"
+yum install curl wget -y 1> /dev/null
+yum update curl wget ca-certificates -y 1> /dev/null
+SERVER_OS="RockyLinux"
 elif echo $OUTPUT | grep -q "Ubuntu 18.04" ; then
 apt install -y -qq wget curl
                 SERVER_OS="Ubuntu"
 elif echo $OUTPUT | grep -q "Ubuntu 20.04" ; then
 apt install -y -qq wget curl
-                SERVER_OS="Ubuntu"
+                SERVER_OS="Ubuntu20"
 else
 
                 echo -e "\nUnable to detect your OS...\n"
-                echo -e "\nCyberPanel is supported on Ubuntu 18.04, CentOS 7.x and CloudLinux 7.x...\n"
+                echo -e "\nCyberPanel is supported on Ubuntu 18.04, Ubuntu 20.04, Ubuntu 20.10, CentOS 7.x, CentOS 8.x,  CloudLinux 7.x, CloudLinux 8.x and AlmaLinux 8.x...\n"
                 exit 1
 fi
 
