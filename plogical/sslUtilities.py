265beae0adf4984268e478d7f4dc1fd53503a984
tbaldur <97204751+tbaldur@users.noreply.github.com>
2022-08-30 20:15:53 +0100

    Remove "www" issuing
    Remove certificate issuing for "www".
    It's 2022, no one uses it.

--- plogical/sslUtilities.py
+++ plogical/sslUtilities.py
@@ -336,7 +336,8 @@ class sslUtilities:
                     subprocess.call(shlex.split(command))
 
                 try:
-                    command = acmePath + " --issue -d " + virtualHostName + " -d www." + virtualHostName \
+                    # command = acmePath + " --issue -d " + virtualHostName + " -d www." + virtualHostName \
+                    command = acmePath + " --issue -d " + virtualHostName \
                               + ' --cert-file ' + existingCertPath + '/cert.pem' + ' --key-file ' + existingCertPath + '/privkey.pem' \
                               + ' --fullchain-file ' + existingCertPath + '/fullchain.pem' + ' -w /usr/local/lsws/Example/html -k ec-256 --force --server letsencrypt'
 
@@ -390,10 +391,13 @@ class sslUtilities:
 
                 try:
                     logging.CyberCPLogFileWriter.writeToFile(
-                        "Trying to obtain SSL for: " + virtualHostName + ", www." + virtualHostName + ", " + aliasDomain + " and www." + aliasDomain + ",")
+                          "Trying to obtain SSL for: " + virtualHostName + ", " + aliasDomain ",")
+                        # "Trying to obtain SSL for: " + virtualHostName + ", www." + virtualHostName + ", " + aliasDomain + " and www." + aliasDomain + ",")
 
-                    command = acmePath + " --issue -d " + virtualHostName + " -d www." + virtualHostName \
-                              + ' -d ' + aliasDomain + ' -d www.' + aliasDomain\
+                    # command = acmePath + " --issue -d " + virtualHostName + " -d www." + virtualHostName \
+                            # + ' -d ' + aliasDomain + ' -d www.' + aliasDomain\
+                     command = acmePath + " --issue -d " + virtualHostName \
+                              + ' -d ' + aliasDomain \
                               + ' --cert-file ' + existingCertPath + '/cert.pem' + ' --key-file ' + existingCertPath + '/privkey.pem' \
                               + ' --fullchain-file ' + existingCertPath + '/fullchain.pem' + ' -w /usr/local/lsws/Example/html -k ec-256 --force --server letsencrypt'
 
